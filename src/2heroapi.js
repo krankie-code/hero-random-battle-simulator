7a521689b330110e6e02b8ccafb2c00d7070a02f
krankie <foresvives10@gmail.com>
2020-07-16 14:22:38 +0200

    'more'

--- src/2heroapi.js
+++ src/2heroapi.js
@@ -5,17 +5,23 @@ function getHeroes(){
     const heroes = document.querySelector('.heroes');
         let randomHeroes = []
 
-        let selectRandom = Math.floor(Math.random()*731)
-        while(randomHeroes.length < 2){
-        randomHeroes.push(selectRandom)
-    }
-            for(var i = 0; i<randomHeroes.length+ 1;i++){
+        while(randomHeroes.length < 10){
+            var selectRandom = Math.floor(Math.random()*731)
+
+            randomHeroes.push(selectRandom)
+        }
+        let counter = 0
+        for(var i = 0; i<randomHeroes.length+ 1;i++){
          fetch(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/599075271045222/${randomHeroes[i]}`)
+
         .then((response) =>{
             return response.json()
         })
-
+    
         .then((data) => {
+            if(data.powerstats.intelligence != "null" && counter < 2){
+            
+           
             const article = document.createElement('article');
             article.innerHTML =`
             <h3>${data.name}</h3> 
@@ -29,14 +35,17 @@ function getHeroes(){
             <li>Combat:${data.powerstats.combat}</li>
             </ul>
             `
-    
-
+            console.log(data)
             heroes.appendChild(article)
-                 })
-            .catch((err) =>{})
-            }
+            counter++
+        }
+        })
+        .catch((err) =>{})
+    }
             
     }
 
         getHeroes();
 
+
+    
